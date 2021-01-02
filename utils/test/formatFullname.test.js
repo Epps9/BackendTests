317c05089700b7d67f137c300d83d557a0960ad0
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-01-02 16:45:26 +0100

    Add formatFullname tests

--- utils/test/formatFullname.test.js
+++ utils/test/formatFullname.test.js
@@ -1,3 +1,23 @@
 const formatFullname = require('../formatFullname');
 const expect = require('chai').expect;
 
+describe ('formatFullname', () => {
+
+    it('returns false if fullName is empty', () => {
+      expect(formatFullname(' ')).to.equal('Error');
+    });
+
+    it ('returns an Error if fullname is not a string', () => {
+    if (typeof fullName !== 'string') return 'Error';
+    expect(formatFullname(undefined)).to.equal('Error');
+    expect(formatFullname(2)).to.equal('Error');
+    expect(formatFullname({})).to.equal('Error');
+    expect(formatFullname([])).to.equal('Error');
+    expect(formatFullname(function () {})).to.equal('Error');
+    });
+
+    it ('returns an Error if fullName format is not firstName, lastName', () => {
+        expect(formatFullname('Ewa')).to.equal('Error');
+        expect(formatFullname('Ewa Braun Berlin')).to.equal('Error');
+    });
+});
